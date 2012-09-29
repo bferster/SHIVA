43ceb31e7d0569f215384cca725e7e0ebba52e78
Bill Ferster <bferster@virginia.edu>
2012-09-29 12:52:50 -0400

    Color chip margin

--- SHIVA_Show.js
+++ SHIVA_Show.js
@@ -1883,7 +1883,6 @@ SHIVA_Show.prototype.DrawLayerControlBox=function(items, show)			// DRAW LAYER C
 		}
 	if (hasGotos)															// If has gotos
 		$("#shcr"+items.length).change( function() { $.proxy(_this.SetLayer(this.id.substr(4),this.checked.toString(),"GoTo"),_this); } );  // Add handler
-
 }
 
 ////////////// CUSTOM OVERLAY //////////////
@@ -2286,7 +2285,7 @@ SHIVA_Show.prototype.SetAttributes=function(props, items, keepData)
   		else if (props[o].opt == "advanced") 
    			str+="<input size='14' tabIndex='-1' onChange='Draw()' onFocus='shivaLib.SetAdvancedAttributes(\""+id+"\",\""+o+"\")' id='"+id+"'/>";
    		else if ((props[o].opt == "color") || (props[o].opt == "colors")) {
-   			str+="<div style='max-height:26px'><input size='14' onChange='Draw()' style='position:relative;text-align:center;height:16px;top:2px' id='"+id+"'/>";
+   			str+="<div style='max-height:26px'><input size='4' onChange='Draw()' style='position:relative;text-align:center;height:16px;top:2px; padding-left: 20px' id='"+id+"'/>";
    			str+="<div style='position:relative;border:1px solid;height:11px;width:11px;top:-16px;left:6px'"
 			if (props[o].opt == "colors")	
   				str+=" onclick='shivaLib.ColorPicker(1,"+i+")' id='"+id+"C'/>";		   			

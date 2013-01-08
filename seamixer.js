6a9543bd50e874d96431453bfd930448b8c9cba5
Bill Ferster <bferster@virginia.edu>
2013-01-08 10:04:24 -0500

    seamixer shell

--- seamixer.js
+++ seamixer.js
@@ -10,13 +10,20 @@ function seaMixer() 												// CONSTRUCTOR
 
 seaMixer.prototype.Run=function(ondoList) 							//	RUN
 {
+	// Do inits
+	window.addEventListener("message",$.proxy(this.ShivaEventHandler,this),false);// Add event listener
 }
 
 seaMixer.prototype.Stop=function() 									//	STOP
 {
+	window.removeEventListener("message",$.proxy(this.ShivaEventHandler,this));	 // Remove event listener
 }
 
-seaMixer.prototype.Do=function(on, act, p1, p2, p3, p4) 				//	DO
+seaMixer.prototype.AddOnDo=function(on, act, p1, p2, p3, p4) 		//	ADD NEW ONDO
+{
+}
+
+seaMixer.prototype.RunOnDo=function(act, p1, p2, p3, p4) 			//	RUN AN INIT ONDO
 {
 }
 
@@ -24,6 +31,15 @@ seaMixer.prototype.Query=function(src, dst, query) 					//	QUERY
 {
 }
 
+seaMixer.prototype.ShivaEventHandler=function(e) 					//	CATCH SHIVA EVENTS
+{
+	trace(e.data);
+}
+
+seaMixer.prototype.SendMessage=function(div, msg) 					//	SEND HTML5 MESSAGE TO DIV
+{
+	div.postMessage(msg,"*");											// Send message to div
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //   QUERY  

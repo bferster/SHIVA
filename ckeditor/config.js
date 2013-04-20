2a33fce462da014ce64cbe2ac16f5511e22d98a5
Bill Ferster <bferster@virginia.edu>
2013-04-19 20:54:40 -0400

    ckedit

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,12 +1,14 @@
 CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
-	    { name: 'clipboard',   groups: [ 'undo' ] },
-	    { name: 'editing',     groups: [ 'spellchecker' ] },
 	    { name: 'links' },
-	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-	    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+	    { name: 'styles'},
 	    { name: 'colors' },
+ 		{ name: 'basicstyles', groups: [ 'basicstyles' ] },
+	    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+   		{ name: 'others'},
 	 	];	
-	config.removeButtons = 'Anchor,Strike,Underline,Subscript,Superscript,Flash,Smiley,Mazimize,Print,Paste,PasteText,Cut,Copy,Iframe,Save,Blocks,CreateDiv,PageBreak';
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+	config.removeButtons='Styles,Format,Anchor,Strike,Underline,Subscript,Superscript,Flash,Smiley,Mazimize,PasteText,Iframe,Save,Blocks,CreateDiv,PageBreak';
+	config.removeDialogTabs='image:advanced;link:advanced';
+	config.removePlugins='elementspath,resize,pastefromword';
+	config.toolbarCanCollapse=true;
 };

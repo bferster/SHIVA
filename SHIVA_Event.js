9bd329ff6387201f9f9e55233657f13345cd6174
Bill Ferster <bferster@virginia.edu>
2017-06-05 09:06:24 -0400

    stagetools link

--- SHIVA_Event.js
+++ SHIVA_Event.js
@@ -808,7 +808,7 @@ SHIVA_Event.prototype.SaveResponse=function(num, val) 					// SAVE RESPONSE TO A
 	if (isNaN(id))	 id=id.replace(/'/g,"\\'")								// Remove apos's
 	if (isNaN(val))	 val=val.replace(/'/g,"\\'")							// Remove apos's
 	if (res.toLowerCase().match(/estore/))									// If saving to eStore
-		$.post("http://www.stagetools.com/shiva/addeasyfile.php",{ email:name, type: "Response", title:id,data:val });
+		$.post("http://www.viseyes.org/shiva/shiva/addeasyfile.php",{ email:name, type: "Response", title:id,data:val });
 }
 
 SHIVA_Event.prototype.HideAll=function(now) 							// HIDE ALL EVENTS
